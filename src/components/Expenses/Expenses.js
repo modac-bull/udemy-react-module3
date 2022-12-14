c2bc5f8944575c8bd6b7e133c0e45836d22b7bc3
jihoon <ljhljh211@naver.com>
2022-12-14 10:49:27 +0900

    git test

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,17 +9,14 @@ import ExpensesFilter from "./ExpensesFilter";
 // State 끌어올리기
 // 데이터 자식 컴포넌트에서 부모 컴포넌트로 보내기
 
-
-
 // key 리액트가 개별 아이템을 인식할 수 있도록 해준다. 고유값을 적어야 한다.
 // 리액트가 key 값으로 개별 아이템을 인식할 수있게 되고
 // 렌더링 할 때 도움을 준다. => 전체 리스트를 다 재 렌더링 할 필요 없이
 // 추가되는 부분에만 렌더링 함
 
 export default function Expenses(props) {
-
   const filterChangeHandler = (selectedYear) => {
-    props.onFilterChange(selectedYear)
+    props.onFilterChange(selectedYear);
   };
 
   return (
