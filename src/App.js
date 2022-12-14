48ddffe8c64f97e7e522d2d2e07a87490f7fbf64
jihoon <ljhljh211@naver.com>
2022-12-14 10:24:46 +0900

    [MOD] task3 정답

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const DUMMY_EXPENSES = [
     id: "e1",
     title: "Car Insurance",
     amount: 294,
-    date: new Date(2021, 2, 28),
+    date: new Date(2022, 2, 28),
   },
   {
     id: "e2",
@@ -22,11 +22,29 @@ const DUMMY_EXPENSES = [
     amount: 294,
     date: new Date(2021, 9, 28),
   },
+  {
+    id: "e4",
+    title: "Car Insurance333",
+    amount: 294,
+    date: new Date(2020, 9, 28),
+  },
+  {
+    id: "e5",
+    title: "Car Insurance333",
+    amount: 294,
+    date: new Date(2019, 9, 28),
+  },
+  {
+    id: "e6",
+    title: "Car Insurance333",
+    amount: 294,
+    date: new Date(2020, 9, 28),
+  },
 ];
 
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-  const [filteredYear, setFilteredYear] = useState("2020");
+  
 
   const addExpenseHandler = (expense) => {
     console.log("in app.js");
@@ -34,29 +52,16 @@ const App = () => {
     // setExpenses([expense, ...expenses]); // 좋은 방법이 아니다.
 
     // 동일한 상태의 이전 스냅샨을 기반으로 하는 경우에 상태를 업데이트를 할 수 있는 깔끔한 방법이다.
-    setExpenses((prevExpenses) => {
+    setExpenses(prevExpenses => {
       return [expense, ...prevExpenses];
-    });
-  };
-
-  const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-
-    let newArr = DUMMY_EXPENSES.filter(
-      (expense) => Number(selectedYear) === expense.date.getFullYear()
-    );
-    setExpenses(newArr);
+    })
   };
 
   // props 키 이름은, 던져준 키값의 이름을 사용해야 한다.
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses
-        items={expenses}
-        onFilterChange={filterChangeHandler}
-        filteredYear={filteredYear}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Expense.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
@@ -9,27 +9,30 @@ import ExpensesFilter from "./ExpensesFilter";
 // State 끌어올리기
 // 데이터 자식 컴포넌트에서 부모 컴포넌트로 보내기
 
-
-
 // key 리액트가 개별 아이템을 인식할 수 있도록 해준다. 고유값을 적어야 한다.
 // 리액트가 key 값으로 개별 아이템을 인식할 수있게 되고
 // 렌더링 할 때 도움을 준다. => 전체 리스트를 다 재 렌더링 할 필요 없이
 // 추가되는 부분에만 렌더링 함
 
 export default function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
-    props.onFilterChange(selectedYear)
+    setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  });
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter
           onChangeFilter={filterChangeHandler}
-          selected={props.filteredYear}
+          selected={filteredYear}
         />
-        {props.items.map((expense) => {
+        {filteredExpenses.map((expense) => {
           return (
             <ExpenseItem
               key={expense.id}
